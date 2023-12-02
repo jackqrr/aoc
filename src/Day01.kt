8cbe8b50d16c5c66b33b09bfeeb0addc8caef8f2
Jacek Rondio <mail.jackr@gmail.com>
2023-12-02 18:27:38 +0100

    Day 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val isDigitRegex = "[0-9]".toRegex()
+    val digits = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    val isDigitOrSpelled = digits.joinToString(separator = "|") { it } + "|\\d"
+    val isDigitOrSpelledRegex = isDigitOrSpelled.toRegex()
+
+    fun part1(input: List<String>): Int =
+            input.fold(0) { acc, line ->
+                acc + isDigitRegex.findAll(line).let {
+                    (it.first().value + it.last().value).toInt()
+                }
+            }
+
+    fun String.getDigit(): String = digits.indexOf(this).let {
+        if (it < 0) this else it.toString()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+            input.fold(0) { acc, line ->
+                acc + line.mapIndexedNotNull{ i,_ -> isDigitOrSpelledRegex.find(line, i) }.let {
+                    (it.first().value.getDigit() + it.last().value.getDigit()).toInt().also { println(it) }
+                }
+            }
+
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
---
+++ src/Day02.kt
@@ -0,0 +1,59 @@
+import java.lang.IllegalArgumentException
+import kotlin.math.max
+
+fun main() {
+
+    fun String.parseGames(startIndex: Int) = substring(startIndex + 1).split(";")
+            .map {
+                it.trim().split(",").associate { cubes ->
+                    cubes.trim().split(" ")
+                            .let { it.last().trim() to it.first().trim().toInt() }
+                }
+            }
+
+    fun part1(input: List<String>): Int =
+            input.fold(0) { acc, line ->
+                val gameIdEndIndex = line.indexOfFirst { it == ':' }
+                if (gameIdEndIndex == -1) throw IllegalArgumentException("Wrong input syntax")
+
+                val gameId = line.substring(5, gameIdEndIndex).toInt()
+
+                val gamePossible = line.parseGames(gameIdEndIndex).all {
+                    val reds = it["red"] ?: 0
+                    val greens = it["green"] ?: 0
+                    val blues = it["blue"] ?: 0
+                    reds <= 12 && greens <= 13 && blues <= 14
+                }
+
+                if (gamePossible) acc + gameId else acc
+            }
+
+    fun part2(input: List<String>): Int =
+            input.fold(0) { acc, line ->
+                val gameIdEndIndex = line.indexOfFirst { it == ':' }
+                if (gameIdEndIndex == -1) throw IllegalArgumentException("Wrong input syntax")
+
+                var reds = 0
+                var greens = 0
+                var blues = 0
+
+                line.parseGames(gameIdEndIndex)
+                        .forEach {
+                            reds = max(reds, it["red"] ?: 0)
+                            greens = max(greens, it["green"] ?: 0)
+                            blues = max(blues, it["blue"] ?: 0)
+                        }
+
+                acc + (reds * greens * blues)
+            }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 8)
+    val testInput2 = readInput("Day02_test")
+    check(part2(testInput2) == 2286)
+
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
